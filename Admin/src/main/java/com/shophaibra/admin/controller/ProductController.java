5a6c51a6b465a29633778b4432cbe6e43416fa43
DESKTOP-HQ1JJGV\OS <haeahae081099@gmail.com>
2023-04-19 01:05:35 +0700

    CRUD Product

--- Admin/src/main/java/com/shophaibra/admin/controller/ProductController.java
+++ Admin/src/main/java/com/shophaibra/admin/controller/ProductController.java
@@ -2,15 +2,13 @@ package com.shophaibra.admin.controller;
 
 import com.shophaibra.library.dto.ProductDto;
 import com.shophaibra.library.model.Category;
+import com.shophaibra.library.model.Product;
 import com.shophaibra.library.service.CategoryService;
 import com.shophaibra.library.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -60,6 +58,58 @@ public class ProductController {
             attributes.addFlashAttribute("error", "Add Failed!");
         }
 
+        return "redirect:/products";
+    }
+
+    @GetMapping("/update-product/{id}")
+    public String updateProductForm(@PathVariable("id") Long id, Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("title", "Update product");
+        List<Category> categories = categoryService.findAllActivated();
+        ProductDto productDto = productService.getById(id);
+        model.addAttribute("categories", categories);
+        model.addAttribute("productDto", productDto);
+        return "update-product";
+    }
+
+    @PostMapping("/update-product/{id}")
+    public String processUpdate(@PathVariable("id") Long id,
+                                @ModelAttribute("productDto") ProductDto productDto,
+                                @RequestParam("imageProduct") MultipartFile imageProduct,
+                                RedirectAttributes attributes) {
+        try {
+            productService.update(imageProduct, productDto);
+            attributes.addFlashAttribute("success", "Update successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Update Failed!!");
+        }
+        return "redirect:/products";
+    }
+
+    @RequestMapping(value= "/enable-product/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String enabledProduct(@PathVariable("id") Long id, RedirectAttributes attributes) {
+        try {
+            productService.enableById(id);
+            attributes.addFlashAttribute("success", "Enable successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "Enable Failed!");
+        }
+        return "redirect:/products";
+    }
+
+    @RequestMapping(value= "/delete-product/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String deletedProduct(@PathVariable("id") Long id, RedirectAttributes attributes) {
+        try {
+            productService.deleteById(id);
+            attributes.addFlashAttribute("success", "Delete successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "Delete Failed!");
+        }
         return "redirect:/products";
     }
 }
--- Library/src/main/java/com/shophaibra/library/service/impl/ProductServiceImpl.java
+++ Library/src/main/java/com/shophaibra/library/service/impl/ProductServiceImpl.java
@@ -65,24 +65,60 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product update(ProductDto productDto) {
-        Product product = new Product();
-        product.setName(productDto.getName());
-        product.setDescription(productDto.getDescription());
-        product.setCategory(productDto.getCategory());
-        product.setSalePrice(productDto.getSalePrice());
-        product.setCostPrice(product.getCostPrice());
-        product.setCurrentQuantity(product.getCurrentQuantity());
-        return null;
+    public Product update(MultipartFile imageProduct, ProductDto productDto) {
+        try {
+            Product product = productRepository.getById(productDto.getId());
+            if (imageProduct == null) {
+                product.setImage(productDto.getImage());
+            } else {
+                if (imageUpload.checkExisted(imageProduct) == false) {
+                    imageUpload.uploadImage(imageProduct);
+                }
+                product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
+            }
+            product.setName(productDto.getName());
+            product.setDescription(productDto.getDescription());
+            product.setCategory(productDto.getCategory());
+            product.setSalePrice(productDto.getSalePrice());
+            product.setCostPrice(product.getCostPrice());
+            product.setCurrentQuantity(product.getCurrentQuantity());
+            return productRepository.save(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void deleteById(Long id) {
-
+        Product product = productRepository.getById(id);
+        product.set_deleted(true);
+        product.set_activated(false);
+        productRepository.save(product);
     }
 
     @Override
     public void enableById(Long id) {
+        Product product = productRepository.getById(id);
+        product.set_deleted(false);
+        product.set_activated(true);
+        productRepository.save(product);
+    }
 
+    @Override
+    public ProductDto getById(Long id) {
+        Product product = productRepository.getById(id);
+        ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName(product.getName());
+        productDto.setDescription(product.getDescription());
+        productDto.setCurrentQuantity(product.getCurrentQuantity());
+        productDto.setCategory(product.getCategory());
+        productDto.setSalePrice(product.getSalePrice());
+        productDto.setCostPrice(product.getCostPrice());
+        productDto.setImage(product.getImage());
+        productDto.setDeleted(product.is_deleted());
+        productDto.setActivated(product.is_activated());
+        return productDto;
     }
 }
